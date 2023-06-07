49f0b6e27ed28a1787435505f1428c0a9cb3d65f
Frankjrr <35532751+Frankjrr@users.noreply.github.com>
2023-06-07 14:05:27 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credential', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t hassantariq14351/demo-app:java-maven-app-143 .'
+        sh 'docker build -t hassantariq14351/demo-app:aht143 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push hassantariq14351/demo-app:java-maven-app-143'
+        sh 'docker push hassantariq14351/demo-app:aht143'
     }
 } 
 
